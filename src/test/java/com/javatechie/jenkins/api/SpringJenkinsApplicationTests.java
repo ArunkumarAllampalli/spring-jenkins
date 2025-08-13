ef1ff540aea784c5286a630a40407def67d00bdc
arun <allamarunkumar@gmail.com>
2025-08-13 18:20:53 +0530

    second commit

--- src/test/java/com/javatechie/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkins/api/SpringJenkinsApplicationTests.java
@@ -13,7 +13,7 @@ class SpringJenkinsApplicationTests {
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 	@Test
 	void contextLoads() {
-		logger.info("Test Case Executing..");
+		logger.info("===================Test Case Executing=================================..");
 		assertEquals(true, true);
 	}
 
